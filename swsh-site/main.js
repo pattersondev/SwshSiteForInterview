150a2823e51eb35f21de05593a39664c7cf4403c
pattersondev <pattersonrsam@gmail.com>
2023-11-30 00:02:29 -0500

    init

---
+++ swsh-site/main.js
@@ -0,0 +1,9 @@
+import { Gradient } from "./gradient"
+import './style.css'
+
+// Create your instance
+const gradient = new Gradient()
+
+// Call `initGradient` with the selector to your canvas
+
+gradient.initGradient('#gradient-canvas');
